7e68111c9a058930445334ad38babd64b6f94692
Lyric <76908375+4xw@users.noreply.github.com>
2021-01-04 12:16:36 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
   execute(message, client) {
     var helpEmbed = new MessageEmbed()
       .setTitle("Lyric funbot HelpList")
-      .setThumbnail('https://cdn.discordapp.com/avatars/794563301577195541/de7d1c8b363ff7022dc88d7ca834f68d.png?size=1024')
+      .setThumbnail(client.user.displayAvatarUrl())
       .setDescription(`
       **Commands** :
 \`${prefix}iq\` : **iq game**
